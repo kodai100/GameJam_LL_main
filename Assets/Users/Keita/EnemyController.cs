56ff04cb6536fa06d426fecc85fcffba8ebb81dc
asachun <chuchuchu.chunchun@gmail.com>
2017-01-14 14:16:27 +0900

    Implement Enemy::SearchPlayer.

--- Assets/Users/Keita/EnemyController.cs
+++ Assets/Users/Keita/EnemyController.cs
@@ -2,18 +2,34 @@
 
 public class EnemyController : MonoBehaviour
 {
+	[SerializeField]
+	private float m_Visibility;
+
 	private CharacterController m_CharacterController;
+
+	private Transform m_PlayerTransform;
 
 	void Awake()
 	{
 		m_CharacterController = GetComponent<CharacterController>();
+
+		// Fix later.
+		m_PlayerTransform = GameObject.Find("Player").transform;
 	}
 
 	void Update ()
 	{
 		var scale = transform.localScale.x;
 
-		m_CharacterController.SimpleMove(transform.forward * Scale2Speed(scale));
+		if (SearchPlayer())
+		{
+			m_CharacterController.SimpleMove(transform.forward * Scale2Speed(scale));
+		}
+	}
+
+	private bool SearchPlayer()
+	{
+		return Vector3.SqrMagnitude(transform.position - m_PlayerTransform.position) < m_Visibility * m_Visibility;
 	}
 
 	private float Scale2Speed(float scale)
@@ -22,4 +38,12 @@ public class EnemyController : MonoBehaviour
 
 		return rate * scale;
 	}
+
+#if UNITY_EDITOR
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireSphere(transform.position, m_Visibility);
+	}
+#endif
 }
