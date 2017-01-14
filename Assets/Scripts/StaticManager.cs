e6ba6e25314974de5b142d31fb4d3a3d509b2240
Masaya Kobayashi <koayashi03@hotmail.co.jp>
2017-01-14 15:13:46 +0900

    [add]静的変数管理クラス追加

---
+++ Assets/Scripts/StaticManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class StaticManager{
+
+	public static int enemyCount = 0;
+
+	// 値の初期化
+	public static void init(){
+		enemyCount = 0;
+	}
+
+}
