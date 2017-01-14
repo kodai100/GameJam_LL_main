8caaf755c90ed114c25c381eca6e52fdb3e1f5fc
asachun <chuchuchu.chunchun@gmail.com>
2017-01-14 13:57:02 +0900

    Add Enemy Components.

---
+++ Assets/Users/Keita/EnemyController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+	private CharacterController m_CharacterController;
+
+	void Awake()
+	{
+		m_CharacterController = GetComponent<CharacterController>();
+	}
+
+	void Update ()
+	{
+		m_CharacterController.SimpleMove(transform.forward);
+	}
+}
