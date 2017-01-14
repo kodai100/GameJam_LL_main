e598f76ba62dd04c2dde0e9d2485a0c7e0fc4b93
asachun <chuchuchu.chunchun@gmail.com>
2017-01-14 15:18:21 +0900

    Add EnemySpawner.

---
+++ Assets/Users/Keita/EnemySpawner.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+#if UNITY_EDITOR
+	[SerializeField]
+	private bool m_ShowSpawnArea;
+#endif
+
+	[SerializeField]
+	private Vector3 m_SpawnAreaCenter;
+
+	[SerializeField]
+	private Vector3 m_SpawnAreaSize;
+
+	[SerializeField]
+	private float m_SafetyRange;
+
+	[SerializeField]
+	private GameObject m_EnemyPrefab;
+
+	private Transform m_PlayerTransform;
+
+	void Awake()
+	{
+		// Fix later.
+		m_PlayerTransform = GameObject.Find("Player").transform;
+	}
+
+#if UNITY_EDITOR
+	void OnDrawGizmos()
+	{
+		if (m_ShowSpawnArea)
+		{
+			Gizmos.color = Color.blue;
+			Gizmos.DrawWireCube(m_SpawnAreaCenter + new Vector3(0f, m_SpawnAreaSize.y / 2f, 0f), m_SpawnAreaSize);
+		}
+	}
+#endif
+}
