fb426719237d35a8cdbc65d5b96fe508d810d52f
morimoto <kfjop17@gmail.com>
2017-01-14 17:12:02 +0900

    add TitleScene, add ResultScene

---
+++ Assets/score.cs
@@ -0,0 +1,37 @@
+﻿/*
+ リザルトのスコア表示
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class score : MonoBehaviour {
+    //void showscore(float score)
+    //{
+        
+    //}
+
+    //float time = 0;
+
+    //float ResultScore = MainGameController. getScore()
+
+    float ResultScore = 200;
+    float i = 0;
+
+	void Update () {
+
+        if (i < ResultScore)
+        {
+            i++;
+        }
+
+        this.GetComponent<Text>().text = "score" + i;
+
+        //time += Time.deltaTime;
+        //Debug.Log(time);
+        //this.GetComponent<Text>().text = "score" + time;
+	}
+}
---
+++ Assets/tireController.cs
@@ -0,0 +1,19 @@
+﻿/*
+ タイトルからリザルトへの遷移
+*/
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class tireController : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Result");
+        }
+    }
+
+}
