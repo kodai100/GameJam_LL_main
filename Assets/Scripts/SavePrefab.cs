83f12710f243e647b749e02fc1181b2bca2177b1
KodaiTakao <takaokodai@team-lab.com>
2017-01-14 12:51:24 +0900

    Initial Commit

---
+++ Assets/Scripts/SavePrefab.cs
@@ -0,0 +1,17 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+
+public class Util : Editor {
+
+    [UnityEditor.MenuItem("Edit/SavePrefab %&s")]
+
+    static void SavePrefab() {
+
+        AssetDatabase.SaveAssets();
+
+    }
+
+}
+
+#endif
