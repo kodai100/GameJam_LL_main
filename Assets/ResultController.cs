7c9014a59f1a48dc64a478264ea7d2bc1228d233
morimoto <kfjop17@gmail.com>
2017-01-14 20:37:09 +0900

    result time

---
+++ Assets/ResultController.cs
@@ -0,0 +1,32 @@
+﻿
+/*
+　リザルトからタイトルへの遷移処理
+*/
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ResultController : MonoBehaviour
+{
+    float time = 0;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Title");
+        }
+
+        time += Time.deltaTime;
+
+        if(time >= 10)
+        {
+            time = 0;
+
+            SceneManager.LoadScene("Title");
+
+        }
+    }
+
+}
---
+++ Assets/TitleController.cs
@@ -0,0 +1,19 @@
+﻿/*
+ タイトルからメインへの遷移
+*/
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TitleController : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+
+}
