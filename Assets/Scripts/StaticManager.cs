943c596c0ad5e35a94f69d1a53de1e9f2eae38ff
asachun <chuchuchu.chunchun@gmail.com>
2017-01-15 12:21:00 +0900

    Fix Enemy move.

--- Assets/Scripts/StaticManager.cs
+++ Assets/Scripts/StaticManager.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine;
 
 public class StaticManager{
 
 	public static int enemyCount = 0;
 
 	public static float gameLength = 60f;
+
+	public static Vector3 playerPos = Vector3.zero;
 
 	public static float playerScale = 0f;
 
