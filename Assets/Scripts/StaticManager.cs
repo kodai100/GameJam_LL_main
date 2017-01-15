74532411fca9afd8fa6387ad70933eb1a5c42b65
asachun <chuchuchu.chunchun@gmail.com>
2017-01-15 12:00:19 +0900

    Fix GameLength.

--- Assets/Scripts/StaticManager.cs
+++ Assets/Scripts/StaticManager.cs
@@ -5,7 +5,7 @@ public class StaticManager{
 
 	public static int enemyCount = 0;
 
-	public static float gameLength = 5f;
+	public static float gameLength = 60f;
 
 	public static float playerScale = 0f;
 
