79362e4f18153650fdaa60a368db0477f254b320
asachun <chuchuchu.chunchun@gmail.com>
2017-01-15 09:17:09 +0900

    Add ApplicationController.

---
+++ Assets/Scripts/Module/ApplicationController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ApplicationController : MonoBehaviour
+{
+	void Start ()
+	{
+		if (FindObjectsOfType<ApplicationController>().Length > 1)
+		{
+			DestroyImmediate(gameObject);
+		}
+		else
+		{
+			DontDestroyOnLoad(gameObject);
+		}
+	}
+}
