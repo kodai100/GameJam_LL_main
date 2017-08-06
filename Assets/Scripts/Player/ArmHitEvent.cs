1cb58bd0880b23cdf4685e07ee6fa23a0c0204fb
小林将也 <kobayashi@kobayashi.local>
2017-08-07 01:07:05 +0900

    作業環境を変えるためのコミット

---
+++ Assets/Scripts/Player/ArmHitEvent.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ArmHitEvent : MonoBehaviour {
+
+	#region private variables
+	List<GameObject> mCatchLists = new List<GameObject>();
+	#endregion
+
+
+	void Update(){
+		
+		foreach (GameObject enemy in mCatchLists) {
+			enemy.transform.position = transform.position;
+		}
+	}
+		
+	void OnTriggerStay(Collider other) {
+
+		// 敵を捕まえてプレイヤーのところに持ってくる処理
+		if (other.gameObject.tag == "Enemy") {
+
+			mCatchLists.Add (other.gameObject);
+			other.gameObject.GetComponent<Collider> ().enabled = false;
+		}
+
+		// 障害物に当たったら舌を引っ込める処理に遷移
+		else if (other.gameObject.tag != "Player") {
+
+			gameObject.transform.parent.GetComponent<Shoot> ().returnArm ();
+		}
+			
+	}
+
+	// 敵をDestroyするときこれを呼ばなければいけない(該当する敵オブジェクトがなくてもエラーはでない)
+	void DeleteEnemyFromLists(GameObject destroyed){
+
+		foreach (GameObject enemy in mCatchLists) {
+			if (destroyed == enemy) {
+				mCatchLists.Remove (enemy);
+				break;
+			}
+		}
+	}
+	#endregion
+
+}
--- Assets/Scripts/Player/Shoot.cs
+++ Assets/Scripts/Player/Shoot.cs
@@ -9,11 +9,11 @@ public class Shoot : MonoBehaviour {
     public GameObject arm;
     public float maxArmDistance = 10f;
     [HideInInspector] public bool isShooting;
-    public bool isDebug = false;
     #endregion public
 
     #region private
     Camera playerCamera;
+	bool forward = false;
 
     Material playerMaterial;
 
@@ -40,65 +40,10 @@ public class Shoot : MonoBehaviour {
 				float distance = maxArmDistance;
 				arm.transform.LookAt (transform.position + dir * distance);
 				StartCoroutine(shootAnimation (dir, distance));
-
-				/*
-				// カメラから画面の中心の空間に対してRayを飛ばす
-                RaycastHit hit;
-                Ray ray = playerCamera.ScreenPointToRay(new Vector2(Screen.width * 0.5f, Screen.height * 0.5f));
-
-                if (Physics.Raycast(ray, out hit)) {
-                    
-                    if (hit.collider.gameObject.tag == "Enemy") {
-                        Debug.Log("Enemy Hit");
-
-                        GameObject enemy = hit.collider.gameObject;
-
-                        dir = Vector3.Normalize(enemy.transform.position - transform.position);
-                        distance = Vector3.Distance(enemy.transform.position, transform.position);
-
-                        if(distance < maxArmDistance) {
-                            nextPlayerColor = enemy.GetComponent<Renderer>().material.GetColor("_Color");
-                            arm.transform.LookAt(enemy.transform.position);
-                            StartCoroutine(shootAnimationAndDestroy(enemy));
-                        }
-                        
-					} else if(hit.collider.gameObject.tag != "Player"){
-                        dir = Vector3.Normalize(hit.point - transform.position);
-                        distance = Vector3.Distance(hit.point, transform.position);
-
-                        if (distance < maxArmDistance) {
-                            arm.transform.LookAt(hit.point);
-                            StartCoroutine(shootAnimation());
-                        }
-                        
-                    }
-
-                } 
-				else { // Rayがヒットしない場合でも空間に対して舌を伸ばすように処理
-                    dir = Vector3.Normalize(playerCamera.ScreenToWorldPoint(Input.mousePosition) - transform.position);
-                    distance = Vector3.Distance(playerCamera.ScreenToWorldPoint(Input.mousePosition), transform.position);
-                    arm.transform.LookAt(playerCamera.ScreenToWorldPoint(Input.mousePosition));
-                    StartCoroutine(shootAnimation());
-                }
-				*/
             }
 
         }
     }
-
-	/*
-    void OnDrawGizmos() {
-        Gizmos.DrawLine(transform.position, transform.position + currentLength * dir);
-        Gizmos.DrawWireSphere(transform.position, maxArmDistance);
-    }
-
-    void OnGUI() {
-        if (isDebug) {
-            GUILayout.Label("Defeated Enemies : " + StaticManager.enemyCount);
-        }
-
-    }
-	*/
 
     #endregion MonoBehaviourFuncs
 
@@ -107,9 +52,9 @@ public class Shoot : MonoBehaviour {
         SeManager.Instance.Play("Tongue");
 
 		Vector3 defaultLocalScale = arm.transform.localScale;
-        isShooting 		= true;
-        float currentLength 	= 0f;
-        bool forward 	= true;
+		float currentLength 	= 0f;
+		isShooting 	= true;
+        forward 	= true;
         
 		// 伸びる
         while (currentLength < distance && forward) {
@@ -140,5 +85,10 @@ public class Shoot : MonoBehaviour {
 
         yield break;
     }
-    
+ 
+	// Armを伸ばしていた場合、引っ込める
+	public void returnArm(){
+		forward = false;
+	}
+
 }
