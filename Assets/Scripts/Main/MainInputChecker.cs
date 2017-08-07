dccebade0dbca930b0f3b7d97c20b3d68fe0b599
Masaya_Kobayashi <kobayashi03@hotmail.co.jp>
2017-08-08 08:46:56 +0900

    [add]Mainシーン中にESCキーを押すとゲームを終了する機能を追加

---
+++ Assets/Scripts/Main/MainInputChecker.cs
@@ -0,0 +1,18 @@
+﻿// Mainシーン中の他オブジェクトが管理していない入力を処理
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainInputChecker : MonoBehaviour {
+    
+	// Update is called once per frame
+	void Update () {
+
+        // ESCキーで終了
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            Debug.Log("End");
+            Application.Quit();
+        }
+    }
+}
